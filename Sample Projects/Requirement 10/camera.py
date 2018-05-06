5554b3ce99290dbc073cef4a390c0db1f9ce3c82
ROHIT KUMAR <35887316+11RO05@users.noreply.github.com>
2018-05-06 22:55:24 +0500

    src file

---
+++ Sample Projects/Requirement 10/camera.py
@@ -0,0 +1,44 @@
+import P3picam
+import picamera
+from datetime import datetime
+from subprocess import call
+
+motionState = False
+picPath = "/home/pi/"     # Set your desired location. this is where the captured images will be saved
+
+def captureImage(currentTime, picPath):
+    # Generate the name of the picture's name
+    picName = currentTime.strftime("%Y.%m.%d-%H%M%S") + '.jpg'
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.capture(picPath + picName)
+    print("We have taken a picture")
+    return picName
+
+def getTime():
+    #Fetch the current time
+    currentTime = datetime.now()
+    return currentTime
+
+
+def timeStamp(currentTime, picPath, picName):
+    # Variable for file path
+    filepath = picPath + picName
+    
+    # Create message to stamp on picture
+    message = currentTime.strftime("%Y.%m.%d - %H:%M:%S")
+    
+    # Create command to execute
+    timestampCommand = "/usr/bin/convert " + filepath + " -pointsize 36 \
+    -fill red -annotate +700+650 '" + message + "'  " + filepath
+    
+    # Execute the command
+    call([timestampCommand], shell=True)
+
+while True:
+    motionState = P3picam.motion()
+    print(motionState)
+    if motionState:
+        currentTime = getTime()
+        picName = captureImage(currentTime, picPath)
+        timeStamp(currentTime, picPath, picName)
