25de1b04c97e18eb53d89726c23240458ab42e0c
ROHIT KUMAR <35887316+11RO05@users.noreply.github.com>
2018-06-27 13:38:51 +0530

    Raindrop.py

--- New Sensors/src/Raindrop.py
+++ New Sensors/src/Raindrop.py
@@ -1,23 +1,21 @@
-# raindrop sensor DO connected to GPIO18
-# HIGH = no rain, LOW = rain detected
-# Buzzer on GPIO13
+### RAIN ALERT SYSTEM ###
+
+#Import the required libraries
 from time import sleep
 from gpiozero import Buzzer, InputDevice
  
-buzz    = Buzzer(13)
-no_rain = InputDevice(18)
+buzz    = Buzzer(13)        # Buzzer at GPIO 13
+no_rain = InputDevice(18)   # Digital pin at GPIO 18
  
-def buzz_now(iterations):
-    for x in range(iterations):
+def buzz_now(iterations):        # User Defined Function with one argument
+    for x in range(iterations):  # For loop for the buzzer to buzz
         buzz.on()
         sleep(0.1)
         buzz.off()
         sleep(0.1)
  
-while True:
-    if not no_rain.is_active:
-        print("It's raining - Get Inside")
-        buzz_now(5)
-        # insert your other code or functions here
-        # e.g. tweet, SMS, email, take a photo etc.
+while True:                                 # Loop Forever
+    if not no_rain.is_active:               # HIGH = no rain, LOW = rain detected
+        print("It's raining - Get Inside")  # Print the message on the screen
+        buzz_now(3)                         # Buzz for 3 times
     sleep(1)
